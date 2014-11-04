e14c3c11f472c8007f2b0a9c3c4e2632edea933e
Hitesh Joshi <me@hiteshjoshi.com>
2014-11-05 02:50:02 +0530

    adding congig for mongodb and mysql

--- server.js
+++ server.js
@@ -5,27 +5,37 @@
 
 var fs = require('fs');
 var express = require('express');
-var mongoose = require('mongoose');
 var passport = require('passport');
 var config = require('config');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-// Connect to mongodb
-var connect = function () {
-  var options = { server: { socketOptions: { keepAlive: 1 } } };
-  mongoose.connect(config.db, options);
+var useMongo = function(){
+	var mongoose = require('mongoose');
+	// Connect to mongodb
+	var connect = function () {
+	  var options = { server: { socketOptions: { keepAlive: 1 } } };
+	  mongoose.connect(config.db, options);
+	};
+	connect();
+
+	mongoose.connection.on('error', console.log);
+	mongoose.connection.on('disconnected', connect);
 };
-connect();
 
-mongoose.connection.on('error', console.log);
-mongoose.connection.on('disconnected', connect);
 
-// Bootstrap models
-fs.readdirSync(__dirname + '/app/models').forEach(function (file) {
-  if (~file.indexOf('.js')) require(__dirname + '/app/models/' + file);
-});
+
+//if we need mongodb support?
+if(config.mongo)
+	useMongo();
+
+
+// Bootstrap mongodb models
+if(config.mongo)
+	fs.readdirSync(__dirname + '/app/models/mongodb/').forEach(function (file) {
+	  if (~file.indexOf('.js')) require(__dirname + '/app/models/' + file);
+	});
 
 // Bootstrap passport config
 require('./config/passport')(passport, config);
